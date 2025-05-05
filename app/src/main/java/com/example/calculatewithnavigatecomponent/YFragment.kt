8057a2a7083d8c7487c59712b8d141452339db1a
theDone <done.muzaffer5@gmail.com>
2025-05-05 23:07:12 +0300

    eklenen butonlarla sayfa gecisleri saglandı ve geri tusu aktif edildi.

--- app/src/main/java/com/example/calculatewithnavigatecomponent/YFragment.kt
+++ app/src/main/java/com/example/calculatewithnavigatecomponent/YFragment.kt
@@ -5,55 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
+import com.example.calculatewithnavigatecomponent.databinding.FragmentYBinding
+import com.google.android.material.snackbar.Snackbar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [YFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class YFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding:FragmentYBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_y, container, false)
+
+        _binding = FragmentYBinding.inflate(inflater,container,false)
+
+
+
+
+        val backPress = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                Snackbar.make(binding.headerTitleY, "Do you want to go to Home Page?", Snackbar.LENGTH_SHORT)
+                    .setAction("Yes") {
+                        findNavController().popBackStack(R.id.home,false)
+                    }.show()
+            }
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, backPress)
+
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment YFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            YFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
+
 }
